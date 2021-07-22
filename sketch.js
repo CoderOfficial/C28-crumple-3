a9fed1521a0899d3059141ac1200dcb3dcaed6d6
CoderOfficial <84549680+CoderOfficial@users.noreply.github.com>
2021-07-22 14:53:40 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var paper1,ground,bin1,bin2,bin3;
+var engine, world;
+var ground1, edges;
+var dusty
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(800, 400);
+	engine = Engine.create();
+	world = engine.world;
+  
+  ground1 =new Ground(400,500,1000,0.1,)
+ ground1.shapeColor=color(10,0,10)
+
+ bin1=new Bin(400,310)
+ paper1=new Paper(10,10);
+ launcher = new Launcher(paper1.body,{x:200, y:100})
+
+	Engine.run(engine);
+  
+}
+
+
+function draw(){
+  background(255);
+  Engine.update(engine);
+
+  paper1.display();
+  ground1.display();
+  bin1.display();
+  
+launcher.display();
+ 
+}
+
+
+function mouseDragged()
+{
+	Matter.Body.setPosition(paper1.body, {x:mouseX, y:mouseY}) 
+}
+
+function mouseReleased()
+{
+	launcher.fly();
+    // distance=int(dist(stoneObj.x,stoneObj.y,mango1.x,mango1.y));
+}
+
+
